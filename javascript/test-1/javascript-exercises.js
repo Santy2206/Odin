0faef57e7f4fcbe1c215ddc8c824e608172be0e5
Santy2206 <153763536+Santy2206@users.noreply.github.com>
2024-06-06 12:57:46 -0500

    <feat><add a new repository to add my files of the odin project >

---
+++ javascript/test-1/javascript-exercises.js
@@ -0,0 +1,32 @@
+function add7(number){
+
+return number+7
+  
+
+}
+function multiply(a,b)  let result=a*b
+   return result
+}
+function capitalize(a) {
+
+  let index=a.charAt(0)
+
+  let result=index.toUpperCase(2)
+
+
+return result
+ 
+
+}
+
+function lastletter(b) {
+
+ let abstract= b.at(-1)
+ return result=abstract.toUpperCase()
+
+
+}
+
+ 
+
+ console.log(lastletter("xxx"))
