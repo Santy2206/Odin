720532718b38bd06b0f74a2401aa4f8db5c205eb
Santy2206 <153763536+Santy2206@users.noreply.github.com>
2024-06-14 15:26:31 -0500

    <feat > <Write the logic to play a single round> show the answer of each player in the console create a function that calculate the values who wins add 1 to each score to the winner Show the winner and scores

--- javascript/Rock-Paper-Scissors/Rock-Paper-Scissors.js
+++ javascript/Rock-Paper-Scissors/Rock-Paper-Scissors.js
@@ -1,85 +1,115 @@
 
-// - create the computerchoice 
-function getComputerChoice(){
-  let random= Math.random()*100;
+// - Create The Computerchoice 
+  function getComputerChoice(){
+  let random= Math.floor(Math.random()*(2-0+1));
   
-  if(random >=0&&random<=33.333333333){
-   console.log("COMPUTER CHOOSE ROCK")
+  if(random=0){
+   computerwordchoose=("ROCK")
    return random
   }
-  else if(random>=33.333333333&&random<=66.666666666){
-    console.log("COMPUTER CHOOSE PAPER")
+  else if(random=1){
+    computerwordchoose=("PAPER")
     return random
  
   }
-  else if(random>=66.666666666&&random<=100){
-    console.log("COMPUTER CHOOSE SCISSORS")
+  else if(random=2){
+    computerwordchoose=("SCISSORS")
     return random
   }
- }
+  }
+//  create the humans choice
+  //  Create The User Imput
+  let answer_Lower=prompt("Rock Paper Or Scissors? ") 
+    // Turn it into uppercase to avoid syntax mistakes
+      function answer_Upper(){
+        return answer_Lower.toUpperCase()
+     }
+  // Turn The Humans Choice Into Number 
+    function getHumanChoice(){
+      // Create A Variable That Store The Result 
+        let numeric_answer
 
-// - create the prompt 
-let answer_Lower=prompt("Rock Paper Or Scissors? ")
-function answer_Upper(){
+      //Create A Function That Turn The Human Choice To A Number 
 
-  return answer_Lower.toUpperCase()
-}
-// - create possible human choice and store it in a variable and turn it to number
+        if(answer_Upper()=="ROCK"){
+        let numeric_answer=0
+        return numeric_answer
+        }
+        else if(answer_Upper()=="PAPER"){
+        let numeric_answer=1
+        return numeric_answer
+        }
+        else if(answer_Upper()=="SCISSORS"){
+          let numeric_answer=2
+          return numeric_answer
+        }
+        else{
+          return "You Write Wrong Try It Again Bro!!"
+        }
 
-function getHumanChoice(){
-
-  let numeric_answer
-    if(answer_Upper()=="ROCK"){
-     let numeric_answer=Math.random()*(33.333333333-0+1)
-    return numeric_answer
     }
-    else if(answer_Upper()=="PAPER"){
-     let numeric_answer=Math.random()*(66.666666666-33.333333333+1)
-    return numeric_answer
-    }
-    else if(answer_Upper()=="SCISSORS"){
-      let numeric_answer=Math.random()*(100-66.666666666+1)
-      return numeric_answer
-    }
-    else{
-      return "You Write Wrong Try It Again Bro!!"
-    }
+// Create The Players Variable Scores 
+  let humanScore=0;
+  let computerScore=0;
+  let winner
+    let a="COMPUTER'S WINS!!"||"HUMAN'S!!"||"EQUAL!!";
 
-}
-// Create the players Variable Scores 
-
-let humanScore=0;
-let computerScore=0;
 
 // Write the logic to play a single round
+  const HumanSelection=getHumanChoice();
+  const ComputerSelection=getComputerChoice();
+  function playRound(HumanSelection,ComputerSelection){
 
-  // create a function that calculate the values who wins and store it in a variable 
-  // - create the window for showing the computer choice and if you win or loss
-  
-  function playRound(){
+    // show the ansewer of each player in the console
+    console.log(`YOU CHOOSE ${answer_Upper()}`)
+    console.log(`COMPUTER CHOOSE ${computerwordchoose}`)
+     
+    // - create a function that calculate the values who wins
 
-    let winner
-
-    if(getComputerChoice()>getHumanChoice()){
-      computerScore= +1;
-      console.log(`YOU CHOOSE ${ answer_Upper()}`)
-      winner="COMPUTER'S WINS!!"
-
-    }
-    else if (getComputerChoice()<getHumanChoice()){
-      humanScore= +1;
-      console.log(`You Choose ${ answer_Upper()}`)
-      winner="HUMAN'S!!"
-    }
-    else if (getComputerChoice()===getHumanChoice()){
-      console.log(`YOU CHOOSE ${ answer_Upper()}`)
-      winner="EQUAL!!"
-    }
-    console.log(winner,`Human's Score It's${humanScore}`,`Computer's Score It's${computerScore}`) 
+      if(getComputerChoice()>getHumanChoice()){
+        // add 1 to each score to the winner
+        computerScore= +1;
+        winner="COMPUTER'S WINS!!"
+      }
+      else if (getComputerChoice()<getHumanChoice()){
+        humanScore= +1;
+        winner="HUMAN'S WINS!!"
+      }
+      else if (getComputerChoice()==3&&getHumanChoice()==1){
+        humanScore= +1;
+        winner="HUMAN'S WINS!!"
+      }
+      else if (getComputerChoice()===getHumanChoice()){
+        winner="EQUAL!!"
+      }
+      else {
+        computerScore= +1;
+        winner="COMPUTER'S WINS!!"
+      }
+    // Show the winner and scores 
+    let end=console.log(winner ,`Human's Score It's${humanScore}`,`Computer's Score It's${computerScore}`) 
   }
 
   // Write the logic to play the entire game
-  // for(i=0,i>=5,i+1){
-  //   playRound()
-  // }
-  console.log(playRound())
+    //   Create a new function named playGame.
+   function playGame(){
+
+    if(winner=a){
+      playRound()
+    }
+    
+    }
+
+  //  Move your playRound function and score variables so that they’re declared inside of the new playGame function
+
+  // Play 5 rounds by calling playRound 5 times.
+  console.log(playGame())
+  
+
+
+
+  
+  
+  
+
+  
